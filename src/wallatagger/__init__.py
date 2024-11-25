c04489dc679f561cef6e65156f7b7d4d344af8c2
Yatri Trivedi <jivanyatra@gmail.com>
2024-11-24 23:40:17 -0500

    added a scope of what to add during import

--- src/wallatagger/__init__.py
+++ src/wallatagger/__init__.py
@@ -1,1 +1,20 @@
 from .wallatagger import *
+
+__all__ = [
+        # vars
+        "log_path",
+        # functions
+        "authenticate",
+        "authenticate_only",
+        "get_entries_list",
+        "get_entry_tags",
+        "get_last_timestamp",
+        "get_new_timestamp",
+        "get_parsing_pattern",
+        "get_reprocess_flag",
+        "load_env_vars",
+        "parse_for_tags",
+        "process_entries",
+        "set_new_timestamp",
+        "update_entry_tags",
+        ]
