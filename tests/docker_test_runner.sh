1040db8533121460f9c9fe451d086fea2275fc5e
Yatri Trivedi <jivanyatra@gmail.com>
2024-11-24 23:39:53 -0500

    added proper docker compose config, changed test runner with comments, extra cleanup for linux/podman, and compose

--- tests/docker_test_runner.sh
+++ tests/docker_test_runner.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 
+# get rid of any podman settings to use it instead of docker
+unset DOCKER_HOST
+
+# clear out any currently running and old containers
 docker compose down
 docker container kill wallabag_test
 docker container rm wallabag_test
 
+# check if fixtures exist in the right places
 if [ ! -d "./data" ]; then
   mkdir -p ./data/db
 fi
@@ -17,6 +22,7 @@ if [ ! -d "./images" ]; then
   mkdir ./images
 fi
 
+# using compose instead of docker run command now, cmd here for ref
 docker compose up -d
 #docker run -d --name wallabag_test \
 #-v ./data:/var/www/wallabag/data \
@@ -27,8 +33,13 @@ docker compose up -d
 # --mount type=bind,source=C:\absolute\path\to\folder,target=/var/www/wallabag/data
 # --mount type=bind,source=C:\absolute\path\to\folder,target=/var/www/wallabag/web/assets/images
 
+# add in custom site parser config
 docker container cp ./fixtures/wired.com.txt wallabag_test:/var/www/wallabag/vendor/j0k3r/graby-site-config/
+# change perms for copied file inside the container
 docker exec -it wallabag_test sh -c "chmod 644 /var/www/wallabag/vendor/j0k3r/graby-site-config/wired.com.txt"
+# change ownership of copied file inside the container
 docker exec -it wallabag_test sh -c "chown nobody:nobody /var/www/wallabag/vendor/j0k3r/graby-site-config/wired.com.txt"
 
+# run tests
 # pipenv run pytest
+uv run pytest
