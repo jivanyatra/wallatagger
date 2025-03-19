74154cc2a97b9e2efd7f92939696f6445e5d9ed2
Yatri Trivedi <jivanyatra@gmail.com>
2025-03-19 01:32:52 -0400

    adjusted imports to be explicitly listed by __all__

--- src/wallatagger/__init__.py
+++ src/wallatagger/__init__.py
@@ -1,20 +1,21 @@
-from .wallatagger import *
+# from .wallatagger import *
 
 __all__ = [
-        # vars
-        "log_path",
-        # functions
-        "authenticate",
-        "authenticate_only",
-        "get_entries_list",
-        "get_entry_tags",
-        "get_last_timestamp",
-        "get_new_timestamp",
-        "get_parsing_pattern",
-        "get_reprocess_flag",
-        "load_env_vars",
-        "parse_for_tags",
-        "process_entries",
-        "set_new_timestamp",
-        "update_entry_tags",
-        ]
+    # vars
+    "log_path",
+    # functions
+    "authenticate",
+    "authenticate_only",
+    "get_entries_list",
+    "get_entry_tags",
+    "get_last_timestamp",
+    "get_new_timestamp",
+    "get_parsing_pattern",
+    "get_reprocess_flag",
+    "load_env_vars",
+    "main",
+    "parse_for_tags",
+    "process_entries",
+    "set_new_timestamp",
+    "update_entry_tags",
+]
